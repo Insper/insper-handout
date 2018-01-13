896ea90c2132399024e8e379643bdb7a4a9b7591
Rafael Corsi <corsiferrao@gmail.com>
2018-01-13 10:44:48 -0200

    primiera versao

---
+++ filterBox.py
@@ -0,0 +1,46 @@
+
+#!/usr/bin/env python
+
+"""
+Pandoc filter to convert divs with class="theorem" to LaTeX
+theorem environments in LaTeX output, and to numbered theorems
+in HTML output.
+"""
+
+from pandocfilters import toJSONFilter, RawBlock, Div
+
+theoremcount = 0
+
+def latex(x):
+    return RawBlock('latex', x)
+
+def html(x):
+    return RawBlock('html', x)
+
+def box(key, value, format, meta):
+    if key == 'Div':
+        [[ident, classes, kvs], contents] = value
+        if "alert" in classes:
+            if format == "latex":
+                if ident == "":
+                    label = ""
+                else:
+                    label = '\\label{' + ident + '}'
+                return([latex('\\begin{boxAlert}' + label)] + contents +
+                       [latex('\\end{boxAlert}')])
+            elif format == "html" or format == "html5":
+                global theoremcount
+                theoremcount = theoremcount + 1
+                newcontents = [html('<dt>Theorem ' + str(theoremcount) + '</dt>'),
+                               html('<dd>')] + contents + [html('</dd>\n</dl>')]
+                return Div([ident, classes, kvs], newcontents)
+        elif "question" in classes:
+                  if format == "latex":
+                      if ident == "":
+                          label = ""
+                      else:
+                          label = '\\label{' + ident + '}'
+                      return([latex('\\begin{boxQuestion}' + label)] + contents +
+                              [latex('\\end{boxQuestion}')])
+if __name__ == "__main__":
+  toJSONFilter(box)
---
+++ toGitHub.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def rreplace(s, old, new, occurrence):
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
+
+def toGitHub(f,o):
+    try :
+        fw = open(o,"w")
+        fr = open(f,"r")
+        box = False
+        pandoc = False
+
+        for line in fr :
+            if "<div" in line:
+                if "alert" in line :
+                    box = True
+                    fw.write("```\n")
+                    fw.write("Info:\n\n")
+                if "question" in line :
+                    box = True
+                    fw.write("```\n")
+                    fw.write("Questão:\n\n")
+            elif "</div" in line :
+                if box == True:
+                    fw.write("```\n")
+            elif "![" in line :
+                if "](" in line :
+                    if ".pdf" in line :
+                        fw.write(rreplace(line,"pdf", "png", 1))
+           # elif "---" in line :
+           #     if pandoc == False :
+           #         if "pandoc" in line[]:
+           #             pandoc = True
+           #     else:
+           #         pandoc = False
+            else:
+                if pandoc == False :
+                    fw.write(line)
+
+    except IOError:
+        print("Arquivo não encontrado")
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--fileIn" , required=True)
+    ap.add_argument("-o", "--fileOut", required=True)
+    args = vars(ap.parse_args())
+    toGitHub(f=args["fileIn"], o=args["fileOut"])
+
+
+

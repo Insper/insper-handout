129a3170275d1d22e0995e4e215fbbd6266d51dd
Igor Montagner <igordsm@gmail.com>
2019-05-20 08:44:31 -0300

    Fix para aceitar versões 1 e 2 do pandoc

--- scripts/insper_handout.py
+++ scripts/insper_handout.py
@@ -6,8 +6,17 @@ import sys
 import os.path
 import argparse
 
-if __name__ == "__main__":
+def check_pandoc_version():
+    resp = subprocess.run(['pandoc', '--version'], stdout=subprocess.PIPE)
+    #print(resp.stdout)
+    linha = str(resp.stdout, 'utf-8').split('\n')[0]
+    version = int(linha.split(' ')[1].split('.')[0])
+    if version < 2:
+        return 'latex-engine'
+    else:
+        return 'pdf-engine'
 
+if __name__ == "__main__":    
     parser = argparse.ArgumentParser(prog="Insper Handout")
     parser.add_argument('filename')
     parser.add_argument('--format', action='store', default='latex')
@@ -21,10 +30,12 @@ if __name__ == "__main__":
 
     script_home = os.path.join(Path.home(), '.insper_handout')
 
+    pdf_engine = check_pandoc_version()
+
     if arguments.format == 'latex':
         if name[-4:] != '.pdf':
             name += '.pdf'
-        cmd_line = 'pandoc -f markdown+tex_math_double_backslash -t latex -o %s %s --data-dir=%s --filter include.py --filter pandoc-svg.py --filter filterBox.py --variable logo=%s/logo.pdf --variable header=%s/cabecalho.png --template %s/InsperTemplate.tex --latex-engine=xelatex'%(name, arguments.filename, script_home, script_home, script_home, script_home )
+        cmd_line = 'pandoc -f markdown+tex_math_double_backslash -t latex -o %s %s --data-dir=%s --filter include.py  --filter filterBox.py --variable logo=%s/logo.pdf --variable header=%s/cabecalho.png --template %s/InsperTemplate.tex --%s=xelatex'%(name, arguments.filename, script_home, script_home, script_home, script_home, pdf_engine )
     elif arguments.format == 'html':
         if name[-5:] != '.html':
             name += '.html'
